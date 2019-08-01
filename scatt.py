9e373caac9cf8fd2309dad093a1a3dddcd577fc8
jerome-colin <51480480+jerome-colin@users.noreply.github.com>
2019-08-01 14:00:13 +0200

    Adding water mask and SRE support

--- scatt.py
+++ scatt.py
@@ -35,6 +35,8 @@ def main():
     parser.add_argument("runA", help="XML file describing run A")
     parser.add_argument("runB", help="XML file describing run A")
     parser.add_argument("-v", "--verbose", help="Set verbosity to INFO level", action="store_true")
+    parser.add_argument("-d", "--diffmap", help="Produce diff map image", action="store_true")
+    parser.add_argument("-m", "--withDTM", help="Produce diff map with MNT alongside", action="store_true")
     parser.add_argument("-r", "--resampling", type=int, \
                         help="Integer n such that products are resampled to 1/n", default=0)
 
@@ -69,21 +71,62 @@ def main():
     # Common pure pixels
     common_pure_pixels = clouds_a.band + clouds_b.band + edge_a.band + edge_b.band
 
+    # Load water mask
+    try:
+        water = A.load_band(name="water mask").resample(n=scaling_n)#.get_finite(mask=common_pure_pixels)
+        common_without_water = common_pure_pixels + water.band
+
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
+
+
     # Scatterplot of AOT
     aot_a_rs = A.load_band(name="aot").resample(n=scaling_n).get_finite(mask=common_pure_pixels)
     aot_b_rs = B.load_band(name="aot").resample(n=scaling_n).get_finite(mask=common_pure_pixels)
+    aot_a_rs_without_water = A.load_band(name="aot").resample(n=scaling_n).get_finite(mask=common_without_water)
+    aot_b_rs_without_water = B.load_band(name="aot").resample(n=scaling_n).get_finite(mask=common_without_water)
 
     majatools.scatterplot(aot_a_rs.band, aot_b_rs.band, \
-                          title     = A.context + " " + A.type + " vs " + B.context + " " + B.type, \
-                          xt        = A.context + " " + aot_a_rs.band_name, \
-                          yt        = B.context + " " + aot_b_rs.band_name, \
-                          f_savefig = A.context + "_" + aot_a_rs.band_name.replace(" ", "-") \
+                          aot_a_rs_without_water.band, aot_b_rs_without_water.band,\
+                          title=A.context + " " + A.type + " vs " + B.context + " " + B.type, \
+                          xt=A.context + " " + aot_a_rs.band_name, \
+                          yt=B.context + " " + aot_b_rs.band_name, \
+                          f_savefig=A.context + "_" + aot_a_rs.band_name.replace(" ", "-") \
                                     + "_vs_" \
                                     + B.context + "_" + aot_b_rs.band_name.replace(" ", "-") \
                                     + ".png"
                           )
 
+    # Scatterplot of SRE
+    for s2band in ("B2","B3","B4"):
+        sre_a_rs = A.load_band(name="sre"+s2band).resample(n=scaling_n).get_finite(mask=common_pure_pixels)
+        sre_b_rs = B.load_band(name="sre"+s2band).resample(n=scaling_n).get_finite(mask=common_pure_pixels)
+        sre_a_without_water = A.load_band(name="sre"+s2band).resample(n=scaling_n).get_finite(mask=common_without_water)
+        sre_b_without_water = B.load_band(name="sre"+s2band).resample(n=scaling_n).get_finite(mask=common_without_water)
+
+        majatools.scatterplot(sre_a_rs.band, sre_b_rs.band, \
+                              sre_a_without_water.band, sre_b_without_water.band,\
+                              title=A.context + " " + A.type + " vs " + B.context + " " + B.type, \
+                              xt=A.context + " " + sre_a_rs.band_name, \
+                              yt=B.context + " " + sre_b_rs.band_name, \
+                              f_savefig=A.context + "_" + sre_a_rs.band_name.replace(" ", "-") \
+                                        + "_vs_" \
+                                        + B.context + "_" + sre_b_rs.band_name.replace(" ", "-") \
+                                        + ".png", \
+                                        mode="sre"
+                              )
+
+
+    if args.diffmap:
+        if args.withDTM:
+            majatools.diffmap(A, B, "aot", with_dtm=True)
+        else:
+            majatools.diffmap(A, B)
+
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
